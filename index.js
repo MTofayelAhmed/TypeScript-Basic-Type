93676afa09cb64adbe54b76bf6b3344506e80611
MTofayelAhmed <tofayelahmed324@gmail.com>
2024-04-27 22:44:01 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Build IN Type
+var firstName;
+var lastName;
+var fullName;
+var userId;
+var isActivated;
+firstName = "Tofayel";
+lastName = " Ahmed";
+fullName = firstName.concat(lastName);
+userId = 1111;
+isActivated = false;
+console.log("Your Id is ".concat(userId, " userName is ").concat(fullName, " "));
+console.log(fullName.split(" "));
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
---
+++ index.ts
@@ -0,0 +1,21 @@
+// Build IN Type
+
+let firstName: string;
+let lastName: string;
+let fullName: string;
+let userId: number;
+let isActivated: boolean;
+
+firstName= "Tofayel";
+lastName =" Ahmed";
+
+fullName= firstName.concat(lastName)
+
+
+
+userId = 1111;
+isActivated = false;
+console.log(`Your Id is ${userId} userName is ${fullName} `)
+console.log(fullName.split(" "))
+console.log(fullName.toUpperCase())
+console.log(fullName.toLowerCase())
